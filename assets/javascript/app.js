84ae80d9d2fbcf990ec9288238dff3a36b0aef13
Hannah Willis <han.e.willis@gmail.com>
2018-11-24 08:26:44 -0600

    correct api key

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,84 +1,109 @@
-var sports = ["Soccer", "Football", "Surfing", "Sailing", "Basketball"];
+var topics = {
+  searchSport: "",
+  sports: [
+    "Soccer",
+    "Basketball",
+    "Baseball",
+    "Surfing",
+    "Badminton",
+    "Bowling"
+  ]
+};
 
-$(document).on("click", ".topicTop", function() {
-  var sport = $(this).attr("data-topic");
-
-  var queryURL =
-    "https://api.giphy.com/v1/gifs/search?q=" +
-    sport +
-    "&api_key=gm0Sioy3qFVRdnVKyvQeuxfFAsWFMGf7";
-
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).done(function(response) {
-    console.log(response);
-    var results = response.data;
-
-    $("#allSports").empty();
-
-    for (var i = 0; i < results.length; i++) {
-      var sportsDiv = $('<div class="topic pull-left">');
-      var p = $("<p>").text("Rating: " + results[i].rating);
-      var sportImage = $("<img>");
-
-      sportImage.attr("src", results[i].images.fixed_height_still.url);
-
-      sportImage.addClass("gif");
-
-      sportImage.attr("data-state", "still");
-
-      sportImage.attr("data-animate", results[i].images.fixed_height.url);
-
-      sportImage.attr("data-still", results[i].images.fixed_height_still.url);
-
-      sportssDiv.append(p);
-      sportsDiv.append(sportImage);
-
-      $("#topicsHere").prepend(sportssDiv);
-    }
-  });
-});
-
-function renderButtons() {
-  $("#sportsButtons").empty();
-
-  for (var i = 0; i < sports.length; i++) {
-    var a = $("<button>");
-    a.addClass("topicTop btn btn-default");
-    a.attr("data-topic", sports[i]);
-    a.text(sports[i]);
-    $("#sportsButtons").append(a);
+function init() {
+  for (i = 0; i < topics.sports.length; i++) {
+    console.log(topics.sports[i]);
+    $("#topics").append(
+      "<button class='btn-primary topic-btn'>" + topics.sports[i] + "</button>"
+    );
   }
 }
 
-$(document).ready(function() {
-  renderButtons();
-  $("#addSport").on("click", function() {
-    var newSport = $("#topic-input")
-      .val()
-      .trim();
-    // The topic from the textbox is then added to our array
-    sports.push(newSport);
+function search() {
+  $("#giphy").html("");
+  topics.searchSport = $("#search")
+    .val()
+    .trim();
+  //Set queryURL for AJAX Request
+  var queryURL =
+    "https://api.giphy.com/v1/gifs/search?q=" +
+    topics.searchSport +
+    "&api_key=dc6zaTOxFJmzC&limit=10";
 
-    // The button is then rendered
-    renderButtons();
-    // This erases the input once the button is submit button is clicked
-    $("#topic-input").val("");
-    // this is so that the page doesn't refresh once we click on the 'submit' button
-    return false;
+  //AJAX Request
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).done(response => {
+    console.log(response);
+    for (i = 0; i < response.data.length; i++) {
+      //Add raiting and img to html
+      $("#giphy").append(
+        "<div class= 'gif-div'>Rating: " +
+          response.data[i].rating.toUpperCase() +
+          "<br>" +
+          "<img data-name= " +
+          response.data[i].images.original.url +
+          " src= " +
+          response.data[i].images.original_still.url +
+          " class= 'gif-img'></div>"
+      );
+    }
   });
-});
+  //Empty the countries array & create new button
+  topics.sports = [];
+  //Add new country to the array
+  topics.sports.push(topics.searchSport);
+  //Clear #search
+  $("#search").val("");
+  //Run init()
+  init();
+}
 
-// This function handles the state of the gifs
-$(document).on("click", ".gif", function() {
-  if ($(this).attr("data-state") == "still") {
-    //if the state is still...
-    $(this).attr("src", $(this).data("animate")); //... then change the src to 'animate' (which we get from Giphy API)
-    $(this).attr("data-state", "animate"); //... and change the name of the state to 'animate'
-  } else {
-    //conversely, if the state is 'animate'...
-    $(this).attr("src", $(this).data("still")); // then change the src to 'still' (which we get from Giphy API)
-    $(this).attr("data-state", "still"); // and change the state to 'still'
-  }
-});
+function pressTopicBtn() {
+  $("#giphy").html("");
+  var topicBtnValue = $(this).text();
+  var queryURL =
+    "https://api.giphy.com/v1/gifs/search?q=" +
+    topicBtnValue +
+    "&api_key=gm0Sioy3qFVRdnVKyvQeuxfFAsWFMGf7";
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).done(response => {
+    console.log(response);
+    for (i = 0; i < response.data.length; i++) {
+      //Add raiting and img to html
+      $("#giphy").append(
+        "<div class= 'gif-div'>Rating: " +
+          response.data[i].rating.toUpperCase() +
+          "<br>" +
+          "<img data-name= " +
+          response.data[i].images.original.url +
+          " src= " +
+          response.data[i].images.original_still.url +
+          " class= 'gif-img'></div>"
+      );
+    }
+  });
+}
+
+//In creating each image, I added a data-name containing the gif url. Here I swap that with the still image url being used in the src.
+function changeImage() {
+  var temp = $(this).attr("data-name");
+  $(this).attr("data-name", $(this).attr("src"));
+  $(this).attr("src", temp);
+}
+
+//=======================
+//MAIN PROCESS
+//=======================
+//Initialize on start
+init();
+
+//When the Submit button is clicked the search function is called
+$("#search-btn").on("click", search);
+//When the Country buttons are clicked, the presstopicBtn function is called
+$(document).on("click", ".topic-btn", pressTopicBtn);
+//When the gif images are clicked, changeImage function is called
+$(document).on("click", ".gif-img", changeImage);
